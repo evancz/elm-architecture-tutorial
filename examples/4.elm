cbdae1fb1f61b6216c7b3052b2e9f9ee1ef836d0
Evan Czaplicki <info@elm-lang.org>
2016-04-24 20:23:00 -0700

    Get randomness example working

---
+++ examples/4.elm
@@ -0,0 +1,68 @@
+import Html exposing (..)
+import Html.App as Html
+import Html.Events exposing (..)
+import Random
+
+
+
+main =
+  Html.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+
+
+-- MODEL
+
+
+type alias Model =
+  { dieFace : Int
+  }
+
+
+init : (Model, Cmd Msg)
+init =
+  (Model 1, Cmd.none)
+
+
+
+-- UPDATE
+
+
+type Msg
+  = Roll
+  | NewFace Int
+
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    Roll ->
+      (model, Random.generate NewFace (Random.int 1 6))
+
+    NewFace newFace ->
+      (Model newFace, Cmd.none)
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ h1 [] [ text (toString model.dieFace) ]
+    , button [ onClick Roll ] [ text "Roll" ]
+    ]
