53877338639d25715a2e09a1456eb779bc942146
Evan Czaplicki <evancz@users.noreply.github.com>
2016-09-01 16:57:20 -0700

    Add examples for checkboxes and radio buttons

---
+++ examples/8-checkboxes.elm
@@ -0,0 +1,71 @@
+import Html exposing (Html, fieldset, input, label, text)
+import Html.App as App
+import Html.Attributes exposing (style, type')
+import Html.Events exposing (onClick)
+
+
+
+main =
+  App.beginnerProgram { model = optOut, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model =
+  { notifications : Bool
+  , autoplay : Bool
+  , location : Bool
+  }
+
+
+optOut : Model
+optOut =
+  Model True True True
+
+
+
+-- UPDATE
+
+
+type Msg
+  = ToggleNotifications
+  | ToggleAutoplay
+  | ToggleLocation
+
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    ToggleNotifications ->
+      { model | notifications = not model.notifications }
+
+    ToggleAutoplay ->
+      { model | autoplay = not model.autoplay }
+
+    ToggleLocation ->
+      { model | location = not model.location }
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  fieldset []
+    [ checkbox ToggleNotifications "Email Notifications"
+    , checkbox ToggleAutoplay "Video Autoplay"
+    , checkbox ToggleLocation "Use Location"
+    ]
+
+
+checkbox : msg -> String -> Html msg
+checkbox msg name =
+  label
+    [ style [("padding", "20px")]
+    ]
+    [ input [ type' "checkbox", onClick msg ] []
+    , text name
+    ]
