fe1ce288ea030ee406db44699ff53f7ad2a5b69d
Santosh Joseph <sjoseph@groupon.com>
2016-11-27 22:12:29 -0800

    fix: IMO, the view should not be reversing the string

--- examples/02-field.elm
+++ examples/02-field.elm
@@ -38,7 +38,7 @@ update : Msg -> Model -> Model
 update msg model =
   case msg of
     Change newContent ->
-      { model | content = newContent }
+      { model | content = String.reverse newContent }
 
 
 
@@ -49,5 +49,5 @@ view : Model -> Html Msg
 view model =
   div []
     [ input [ placeholder "Text to reverse", onInput Change ] []
-    , div [] [ text (String.reverse model.content) ]
+    , div [] [ text model.content ]
     ]
