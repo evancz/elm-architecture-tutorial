632774fe64b8cc9f583522968d7a611df5cee330
Anup Dhakal <anupdhakal2000@gmail.com>
2018-09-12 12:48:04 +0545

    Add multiple validations for password and age

--- examples/03-form.elm
+++ examples/03-form.elm
@@ -22,12 +22,13 @@ type alias Model =
     { name : String
     , password : String
     , passwordAgain : String
+    , age : String
     }
 
 
 init : Model
 init =
-    Model "" "" ""
+    Model "" "" "" ""
 
 
 
@@ -38,6 +39,7 @@ type Msg
     = Name String
     | Password String
     | PasswordAgain String
+    | Age String
 
 
 update : Msg -> Model -> Model
@@ -52,6 +54,9 @@ update msg model =
         PasswordAgain password ->
             { model | passwordAgain = password }
 
+        Age age ->
+            { model | age = age }
+
 
 
 -- VIEW
@@ -59,10 +64,12 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
+    Html.form []
         [ viewInput "text" "Name" model.name Name
         , viewInput "password" "Password" model.password Password
         , viewInput "password" "Re-enter Password" model.passwordAgain PasswordAgain
+        , viewInput "text" "Enter your age" model.age Age
+        , viewSubmit "Submit"
         , viewValidation model
         ]
 
@@ -72,10 +79,62 @@ viewInput t p v toMsg =
     input [ type_ t, placeholder p, value v, onInput toMsg ] []
 
 
+viewSubmit : String -> Html msg
+viewSubmit text =
+    input [ type_ "submit", value text ] []
+
+
 viewValidation : Model -> Html msg
 viewValidation model =
-    if model.password == model.passwordAgain then
-        div [ style "color" "green" ] [ text "OK" ]
+    if isNotLongEnough model.password then
+        div [ style "color" "red" ] [ text "Password must be more than 8 characters long!" ]
+
+    else if isNotStrongEnough model.password then
+        div [ style "color" "red" ] [ text "Password must contain upper case, lower case and numeric characters!" ]
+
+    else if passwordsDontMatch model.password model.passwordAgain then
+        div [ style "color" "red" ] [ text "Passwords don't match!" ]
+
+    else if isNotNumber model.age then
+        div [ style "color" "red" ] [ text "Age must be a number!" ]
 
     else
-        div [ style "color" "red" ] [ text "Passwords do not match!" ]
+        div [ style "color" "green" ] [ text "Validations passed!" ]
+
+
+isNotLongEnough : String -> Bool
+isNotLongEnough password =
+    String.length password <= 8
+
+
+isNotStrongEnough : String -> Bool
+isNotStrongEnough password =
+    not <|
+        String.any isUpperCase password
+            && String.any isLowerCase password
+            && String.any isDigit password
+
+
+passwordsDontMatch : String -> String -> Bool
+passwordsDontMatch password passwordAgain =
+    password /= passwordAgain
+
+
+isNotNumber : String -> Bool
+isNotNumber age =
+    String.length age == 0 || not (String.all isDigit age)
+
+
+isUpperCase : Char -> Bool
+isUpperCase char =
+    String.contains (String.fromChar char) "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+isLowerCase : Char -> Bool
+isLowerCase char =
+    String.contains (String.fromChar char) "abcdefghijklmnopqrstuvwxyz"
+
+
+isDigit : Char -> Bool
+isDigit char =
+    String.contains (String.fromChar char) "0123456789"
